2b2b57f34f0c7a01f4e81f0fc74be05a5e5d2eca
MonadoDay <89417555+MonadoDay@users.noreply.github.com>
2021-10-30 23:58:08 -0400

    Add files via upload

--- oddcalc.py
+++ oddcalc.py
@@ -1,6 +1,6 @@
-firstnumber = input("Give me a number, any number"):
-secondnumber = input("Wonderful, now give me another number"):
-operation = input("What operation would you like? (mul, div, mod)"):
+firstnumber = input("Give me a number, any number")
+secondnumber = input("Wonderful, now give me another number")
+operation = input("What operation would you like? (mul, div, mod)")
 if operation == "mul":
         print(firstnumber * secondnumber)
     elif operation == "div":
---
+++ spanish.py
@@ -0,0 +1,9 @@
+english = print("Give me a word in English")
+if english == "cat" || english == "Cat":
+    print("Gato")
+else if english == "dog" || english == "Dog":
+    print("perro")
+else if english == "horse" || english == "Horse":
+    print("caballo")
+else:
+    print("no entiendo")
